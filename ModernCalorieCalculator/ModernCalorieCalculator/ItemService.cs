3d40dc85aa6148cabe10df4d2a05cbb2c3792981
Albert Wagner <albertwagner95@gmail.com>
2020-09-21 21:42:20 +0200

    I created item service, and I created method for adding new product

---
+++ ModernCalorieCalculator/ModernCalorieCalculator/ItemService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModernCalorieCalculator
+{
+    public class ItemService
+    {
+        public List<Item> Items { get; set; }
+
+        public ItemService()
+        {
+            Items = new List<Item>();
+        }
+
+        public ConsoleKeyInfo AddNewItemView(MenuActionService actionService)
+        {
+            var addNewItemMenu = actionService.GetMenuActionsByMenuName("AddNewItemMenu");
+            Console.WriteLine("Please select product category");
+
+            foreach (var menuAction in addNewItemMenu)
+            {
+                Console.WriteLine($"{menuAction.Id}. {menuAction.Name}");
+            }
+
+            var operation = Console.ReadKey();
+
+            return operation;
+        }
+
+        public int AddNewItem(char itemType)
+        {
+            int typeId;
+            int.TryParse(itemType.ToString(), out typeId);
+            Item item = new Item();
+
+
+            return AddNewItemProperties(item, typeId);
+        }
+
+        public int AddNewItemProperties(Item item, int typeId)
+        {
+            item.CategoryId = typeId;
+            item.Id = Items.Count + 1;
+            Console.WriteLine("Please enter name for your new product");
+            var name = Console.ReadLine();
+            Console.WriteLine("Please enter kcal per 100 grams");
+            var kcalPerOneHoundredGramsReading = Console.ReadLine();
+            decimal.TryParse(kcalPerOneHoundredGramsReading, out decimal kcalPerOneHoundredGrams);
+            Console.WriteLine("Please enter quantity fats per one houndred grams");
+            var quantityFatsPerOneHoundredGramsReading = Console.ReadLine();
+            decimal.TryParse(quantityFatsPerOneHoundredGramsReading, out decimal quantityFatsPerOneHoundredGrams);
+
+            item.Name = name;
+            item.KcalPerOneHounderGrams = kcalPerOneHoundredGrams;
+            item.QuantityFatsPOHG = quantityFatsPerOneHoundredGrams;
+
+            Items.Add(item);
+
+            return item.Id;
+        }
+    }
+}
--- ModernCalorieCalculator/ModernCalorieCalculator/Program.cs
+++ ModernCalorieCalculator/ModernCalorieCalculator/Program.cs
@@ -15,21 +15,37 @@ namespace ModernCalorieCalculator
             actionService = Initialize(actionService);
 
             var mainMenu = actionService.GetMenuActionsByMenuName("Main");
-            ShowMenu(mainMenu);
-
-            var operation = Console.ReadKey();
             
-            switch (operation.KeyChar)
-            {
-                case '1':
-                    Console.WriteLine("Adding item.");
-                    break;
 
-                default:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Action you entered does not exist");
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    break;
+
+            ItemService itemService = new ItemService();
+            while (true)
+            {
+                ShowMenu(mainMenu);
+
+                var operation = Console.ReadKey();
+                switch (operation.KeyChar)
+                {
+                    case '1':
+                        var keyInfo = itemService.AddNewItemView(actionService);
+                        var itemId = itemService.AddNewItem(keyInfo.KeyChar);
+
+                        var forShow = itemService.Items.Find(x => x.Id == itemId);
+                        Console.WriteLine( "Pomyślnie dodano" + forShow.Name + forShow.Id);
+
+                        Console.WriteLine("Wszystie produkty");
+                        foreach (var item in itemService.Items)
+                        {
+                            Console.WriteLine(item.Id + " " + item.Name);
+                        }
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Action you entered does not exist");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                }
             }
 
             //Item item = new Item() { TypeOfMeal = KindOfMeal.Breakfast };
@@ -40,6 +56,11 @@ namespace ModernCalorieCalculator
         {
             actionService.AddNewAction(1, "Add new product to database", "Main");
             actionService.AddNewAction(2, "Add product to the selected day", "Main");
+
+            actionService.AddNewAction(1, "Vegetables", "AddNewItemMenu");
+            actionService.AddNewAction(2, "Meal", "AddNewItemMenu");
+            actionService.AddNewAction(3, "Meat", "AddNewItemMenu");
+            actionService.AddNewAction(4, "Fruits", "AddNewItemMenu");
 
             return actionService;
         }
